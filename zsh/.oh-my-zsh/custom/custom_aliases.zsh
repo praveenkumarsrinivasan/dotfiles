b43d038cb6b8ce2c2e338285a4efb59c3f790c8f
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2014-01-17 17:21:12 +0530

    added git aliases and waiting dot in zshrc

--- zsh/.oh-my-zsh/custom/custom_aliases.zsh
+++ zsh/.oh-my-zsh/custom/custom_aliases.zsh
@@ -2,6 +2,22 @@ alias gs='git status'
 alias ga='git add .'
 alias g="git"
 
+alias gp='git push'
+alias gl='git log'
+alias gs='git status'
+alias gd='git diff'
+alias gm='git commit -m'
+alias gma='git commit -am'
+alias gb='git branch'
+alias gc='git checkout'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gpu='git pull'
+alias gcl='git clone'
+alias gta='git tag -a -m'
+alias gf='git reflog'
+alias gh='git hist'
+
 alias gl='git ls-tree --full-tree -r HEAD'
 alias gr='git rm `$(git ls-files --deleted)`'
 
@@ -29,4 +45,5 @@ alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resource
 alias map="xargs -n1"
 
 alias dh='dirs -v'
-
+alias mysql=/usr/local/mysql/bin/mysql
+alias mysqladmin=/usr/local/mysql/bin/mysqladmin

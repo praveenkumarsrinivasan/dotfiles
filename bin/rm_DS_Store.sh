b367ef3163aac50ca817e88dc5d1131f2c40ecdc
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-02-03 19:12:01 +0530

    Adding delete script for DS_Store

---
+++ bin/rm_DS_Store.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# rm_DS_Store.sh
+# Copyright (C) 2015 prempraveen <prempraveen@PremPraveens-MacBook-Pro.local>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+find . -name '*.DS_Store' -type f -delete
